53cbac759c2b299875554e16e515fb279d7b03fc
Andy White <andywhite441@gmail.com>
2021-01-09 20:17:41 -0700

    session and window files

---
+++ layouts/vidagraph-code.window.sh
@@ -0,0 +1,18 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+window_root "~/dev/andywhite37/vidagraph"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+new_window "Code"
+
+split_v 5
+split_h 50
+
+select_pane 1
+run_cmd "nvim src/Main.purs"
+
+select_pane 2
+run_cmd "spago --version"
+
+select_pane 1
